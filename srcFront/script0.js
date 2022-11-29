5fb7f564f45c74dbb2ba86a43d044f59d4608762
Sajahh <mariavaicomoutras@hotmail.com>
2022-11-29 15:39:54 -0300

    teste3

--- srcFront/script0.js
+++ srcFront/script0.js
@@ -5,7 +5,7 @@ const infosForms = document.querySelector('#infosForms')
 infosForms.addEventListener('submit', async event => {
     event.preventDefault();
 
-        await fetch('http://10.44.21.225/login', {
+        await fetch('https://10.44.21.225/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
