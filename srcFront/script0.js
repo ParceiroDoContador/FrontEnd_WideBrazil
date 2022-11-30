f926a687f1681d50bec9a971b94510f887b74c73
Sajahh <mariavaicomoutras@hotmail.com>
2022-11-30 15:41:32 -0300

    testes de deploy 2

--- srcFront/script0.js
+++ srcFront/script0.js
@@ -55,39 +55,3 @@ infosForms.addEventListener('submit', async event => {
 
 
 
-
-
-/*function logar() {
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
-
-    let dados = {
-        email: email,
-        senha: senha
-    }
-    localStorage.setItem("dados", JSON.stringify(dados));
-    console.log(dados);
-    if (!email || !senha) {
-        alert('Preencha todos os campos!')
-    }
-
-    let cabecalho = {
-        method: 'POST',
-        body: JSON.stringify(dados),
-        headers: {
-            'Content-type': 'application/json'
-        }
-    }
-    
-    fetch('http://localhost:8080/srcBack/login', cabecalho)
-        .then(dados => {
-            localStorage.setItem('token', dados.token)
-            window.location.href = 'http://localhost:3000/srcFront/page1.html'
-        }
-        )
-        .catch(erro => {
-            console.log(erro)
-        }
-    )
-}*/
-})
