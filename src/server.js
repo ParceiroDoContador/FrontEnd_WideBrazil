29e5a79301fea77c17818385e55887cabb83a8c8
Sajahh <mariavaicomoutras@hotmail.com>
2022-11-30 19:33:20 -0300

    ajustando para prod

--- src/server.js
+++ src/server.js
@@ -7,8 +7,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use("/static", express.static(__dirname + '/static'));
 app.use(rotas);
 
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running`)
+});
