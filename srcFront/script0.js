af102f4432b5b4e2011e67d943cdbb76e2779b3b
Sajahh <mariavaicomoutras@hotmail.com>
2022-11-30 18:42:35 -0300

    teste 216

--- srcFront/script0.js
+++ srcFront/script0.js
@@ -6,7 +6,7 @@ infosForms.addEventListener('submit', async event => {
     event.preventDefault();
 
     try {
-        await fetch('/login',{
+        await fetch('https://wide-brazil-web-app-pd9vq.ondigitalocean.app/login',{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
