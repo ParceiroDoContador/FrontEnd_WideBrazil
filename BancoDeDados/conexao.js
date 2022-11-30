6a4203dc0067eda196bf90f3fa7aeb0b725c5520
Sajahh <mariavaicomoutras@hotmail.com>
2022-11-30 20:58:09 -0300

    quase pronto

--- BancoDeDados/conexao.js
+++ BancoDeDados/conexao.js
@@ -5,7 +5,7 @@ const knex = require('knex')({
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      ssl: true,
+      ssl: { rejectUnauthorized: false },
       port:process.env.DB_PORT
       
     }
