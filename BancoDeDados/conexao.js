15e1b7d1e572dfc0c4cbd3d085f6dbe4c49c41e8
Sajahh <mariavaicomoutras@hotmail.com>
2022-11-30 11:45:56 -0300

    ajustando para deploy 3

--- BancoDeDados/conexao.js
+++ BancoDeDados/conexao.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-      database: DB_DATABASE,
-      port: DB_PORT
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port:process.env.DB_PORT
       
     }
   })
