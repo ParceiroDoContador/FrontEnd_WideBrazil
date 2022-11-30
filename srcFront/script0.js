9352869778ecb790959c783ab0a7ba9b122e7b69
Sajahh <mariavaicomoutras@hotmail.com>
2022-11-30 16:39:05 -0300

    teste de deploy 4

--- srcFront/script0.js
+++ srcFront/script0.js
@@ -6,7 +6,7 @@ infosForms.addEventListener('submit', async event => {
     event.preventDefault();
 
     try {
-        await fetch('https://wide-brazil-web-app-pd9vq.ondigitalocean.app/srcBack/login',{
+        await fetch('/login',{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
