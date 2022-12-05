09bd83d7249827af59681a2b5c7de2b37878c3c8
Sajahh <mariavaicomoutras@hotmail.com>
2022-12-05 14:34:39 -0300

    testes de integraçao 1

--- src/static/js/script1.js
+++ src/static/js/script1.js
@@ -5,38 +5,11 @@ const valorDolar = document.querySelector('#valorDolar');
 const infosForms = document.querySelector('#infosForms');
 const urlServer = 'https://wide.parceirodocontador.com.br'
 
-downInput.addEventListener('click', async event => {
-    event.preventDefault();
+
+async function downloadInvoice() {
     const token = localStorage.getItem('token');
 
-    if (!token) {
-        alert('Você precisa estar logado para fazer download de arquivos');
-        window.location.href = `${urlServer}/static/page0.html`;
-    }
-    
-    const nome = document.getElementById('nomeCliente').value;
-    const dolar = document.getElementById('valorDolar').value;
-    
-    const { url3 } = await fetch(`${urlServer}/s3UrlPut`, {
-        headers: {
-            authorization: token
-        }
-    }).then(res => res.json());
-
-    await fetch(url3, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'aplication/json',
-        },
-        body: JSON.stringify({ nome, dolar }),
-    });
-
-    const dataUrl3 = url3.split('?')[0];
-    console.log(dataUrl3);
-
-    nomeCliente.value = '';
-    valorDolar.value = '';
-
+try {
     const { url2 } = await fetch(`${urlServer}/s3UrlGet`, {
         headers: {
             authorization: token
@@ -57,4 +30,47 @@ downInput.addEventListener('click', async event => {
         linkRef.download = 'planilha_wide.pdf';
         linkRef.target = '_blank';
         linkRef.click();
-    });
+    
+} catch (error) {
+    console.log(error);
+}
+}
+
+downInput.addEventListener('click', async event => {
+    event.preventDefault();
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+        alert('Você precisa estar logado para fazer download de arquivos');
+        window.location.href = `${urlServer}/static/page0.html`;
+    }
+    
+    const nome = document.getElementById('nomeCliente').value;
+    const dolar = document.getElementById('valorDolar').value;
+
+   /* const {url3} = await fetch(`${urlServer}/s3UrlPut`, {
+        headers: {
+            authorization: token
+        }
+    })
+
+    await fetch(url3, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nome, dolar })
+    }).then(resposta => {})*/
+        
+    await fetch('https://eof0s61ih3lbarl.m.pipedream.net').then(res => {
+        if(res.status === 200) {
+            downloadInvoice();
+        } else {
+            alert('Erro ao baixar arquivo');
+        }
+    })
+
+    nomeCliente.value = '';
+    valorDolar.value = '';
+
+})
