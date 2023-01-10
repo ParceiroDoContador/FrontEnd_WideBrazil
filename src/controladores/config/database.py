5576c5587c8a26d7bf0cb1e3fcaaddbfecb8c027
VinicioSales <vinicio.santtana@gmail.com>
2023-01-10 13:20:59 -0300

    Database

--- src/controladores/config/database.py
+++ src/controladores/config/database.py
@@ -6,8 +6,5 @@ load_dotenv()
 database_infos = {
     "app_key": os.getenv("APP_KEY"),
     "app_secret": os.getenv("APP_SECRET"),
-    "id_conta_corrente": os.getenv("ID_CONTA_CORRENTE"),
-    "teste_app_key": os.getenv("TESTE_APP_KEY"),
-    "teste_app_secret": os.getenv("TESTE_APP_SECRET"),
-    "teste_id_conta_corrente": os.getenv("TESTE_ID_CONTA_CORRENTE")
+    "id_conta_corrente": os.getenv("ID_CONTA_CORRENTE")
 }
